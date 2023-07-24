64273f9e265705e3fa8c5481fd6b9cda91a9c567
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2023-07-24 12:12:25 +0530

    Routing and cart added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
     Link,
 } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
+import CartPage from "./pages/CartPage";
 
 const router = createBrowserRouter([
     {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
     {
         path: "/signup",
         element: <SignupPage />,
+    },
+    {
+        path: "/cart",
+        element: <CartPage />,
     },
 ]);
 
